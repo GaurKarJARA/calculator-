64d52f54f23728a6a6f29843da6f3ab20c1cec1c
unknown <chaturgaurav35@gmail.com>
2025-01-17 13:54:47 +0530

    Addition feature

---
+++ Addition.java
@@ -0,0 +1,15 @@
+package Calculator;
+import java.util.Scanner;
+public class Addition {
+public static void main(String[] args) {
+    Scanner sc  = new Scanner(system.in);
+    System.out.println("enter first number:");
+    int x = sc.nextInt();
+    System.out.println("enter second number:");
+    int y =sc.nextInt();
+    System.out.println("enter third number:");
+    int z = sc.nextInt();
+
+    System.out.println("the sum of given 3 numbers is: "+x+y+z);
+}
+}
