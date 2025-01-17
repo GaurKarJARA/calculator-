bf35a9320454dcc097de70d0f3a68147d113c08e
unknown <chaturgaurav35@gmail.com>
2025-01-17 15:18:03 +0530

    adding multiplication

---
+++ multiplication.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class multiplication {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y =sc.nextInt();
+        System.out.println("multiplication of these 2 numbers are: "+(x*y));
+
+        
+    }
+}
